b27eb934ad01202da0bfc739de755ea58ae753e0
Sayanjyoti Das <sayanjyotidasworkmail@gmail.com>
2021-05-08 20:06:51 +0530

    added binaries

--- core/core.js
+++ core/core.js
@@ -1,4 +1,4 @@
-var keygen = require('ssh-keygen');
+var keygen = require('./keygen/ssh-keygen');
 var publicIp = require('public-ip');
 var fs = require('fs');
 
@@ -8,7 +8,7 @@ module.exports = {
         if (!fs.existsSync('./keys')){
             fs.mkdirSync('./keys');
         }
-        
+
         var location = './keys/key_rsa';
         
         keygen({
